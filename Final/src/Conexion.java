f842131b6f9f354bdb62c57f30284cac4984b2d4
Luisangelteran3489 <37705705+Luisangelteran3489@users.noreply.github.com>
2018-04-03 20:30:52 -0500

    botone

--- Final/src/Conexion.java
+++ Final/src/Conexion.java
@@ -1,3 +1,8 @@
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -8,6 +13,24 @@
  *
  * @author Angel
  */
-public class Mysql {
-    
+public class Conexion {
+  private static Connection cnx = null;
+   public static Connection obtener() throws SQLException, ClassNotFoundException {
+      if (cnx == null) {
+         try {
+            Class.forName("com.mysql.jdbc.Driver");
+            cnx = DriverManager.getConnection("jdbc:mysql://localhost/ejemplo", "root", "");
+         } catch (SQLException ex) {
+            throw new SQLException(ex);
+         } catch (ClassNotFoundException ex) {
+            throw new ClassCastException(ex.getMessage());
+         }
+      }
+      return cnx;
+   }
+   public static void cerrar() throws SQLException {
+      if (cnx != null) {
+         cnx.close();
+      }
+   }
 }
